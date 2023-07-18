8f74fdd9e821f8da814621fda835bea54e96d5d3
MahmoudRefat0 <mahmoudrefat9090@gmail.com>
2023-07-18 11:20:05 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,11 @@
 
 /**
  * add - Adds two values
- * @n1: The frist number to be computed
- * @n2: The second number to be computed
- * Return: Integer value 
+ * @n1: The first number to be computed
+ * @n2: The second value to be computed
+ * Return: Integer value
  */
-
 int add(int n1, int n2)
-
 {
-	return (n1 + n2);
+    return (n1 + n2);
 }

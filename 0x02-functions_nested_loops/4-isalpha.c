65aa36da2b0ea5ff0579f5ee9b83aee702199ad4
MahmoudRefat0 <mahmoudrefat9090@gmail.com>
2023-07-18 06:13:25 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,11 +6,11 @@
  * Return: 1 for alphabetic character or 0 for anything else
  */
 
-int _islower(int c)
+int _islapha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
-		return (1);
+	return (1);
 	}
 	return (0);
 }

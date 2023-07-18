e0687eac2dc4f791029f1e333ecb587101ff0abb
MahmoudRefat0 <mahmoudrefat9090@gmail.com>
2023-07-18 07:21:41 -0700

    5-sign

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -8,7 +8,7 @@
 int main(void)
 {
    	 int r;
-	
+
    	 r = print_sign(98);
    	 _putchar(',');
    	 _putchar(' ');
@@ -25,7 +25,7 @@ int main(void)
    	 _putchar(r + '0');
    	 _putchar('\n');
    	 r = print_sign(-1);
-  	  _putchar(',');
+   	 _putchar(',');
    	 _putchar(' ');
    	 _putchar(r + '0');
    	 _putchar('\n');
